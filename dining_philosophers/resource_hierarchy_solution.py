7bb64377710a327b2a72ae10e8d9c67fee01163b
Lomakin Sergey <71843430+sirejik@users.noreply.github.com>
2020-10-03 00:05:12 +0700

    Add solution for dining philosophers problem

---
+++ dining_philosophers/resource_hierarchy_solution.py
@@ -0,0 +1,21 @@
+import threading
+
+
+def philosopher(left, right):
+    if id(left) < id(right):
+        first, second = left, right
+    else:
+        first, second = right, left
+
+    while True:
+        with first:
+            with second:
+                print('Philosopher with number {} is eating.'.format(threading.currentThread()))
+
+
+fork_number = 5
+forks = [threading.Lock() for n in range(fork_number)]
+philosophers = [
+    threading.Thread(target=philosopher, args=(forks[n], forks[(n + 1) % fork_number])) for n in range(fork_number)
+]
+[x.start() for x in philosophers]
